b708f615d154ae4d6a4eed23641c7ca369cc5604
agsof <bguergachi@gmail.com>
2019-01-15 00:45:08 -0500

    Working Predictor
    -Training Works
    -Simple model
    TODO:
    -Work on mapping
    -Make API for predictor
    -Makre writer to .csv file to retrain

--- Infrastructure/Scheduling/predictor.py
+++ Infrastructure/Scheduling/predictor.py
@@ -10,18 +10,17 @@ import pandas as pd
 import os, sys
 
 
-class predictor:
+class Predictor:
     def __init__(self,dataFolderPath):
+
         self._dataFolderPath = dataFolderPath
         print(tf.__version__)
         sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-        df = pd.read_csv(self._dataFolderPath,na_values=['NA','?'])
+        df = pd.read_csv(self._dataFolderPath+"training.csv",na_values=['NA','?'])
 
         print("DataFrame:\n\n")
         print(df)
-
-
 
         self._nodes = 4
 
@@ -32,29 +31,42 @@ class predictor:
         print(self._y)
 
         self._buildModle()
+        self.train()
 
 
+    #Function to build model
     def _buildModle(self):
-
-
 
         self._model = keras.Sequential()
 
-        self._model.add(keras.layers.Dense(self._nodes, input_dim=self._x.shape[1],activation=tf.nn.relu))
-        self._model.add(keras.layers.Dense(7, kernel_initializer='normal'))
-        self._model.add(keras.layers.Activation(tf.nn.sigmoid))
+        self._model.add(keras.layers.Dense(self._nodes, input_dim= self._x.shape[1],activation=tf.nn.relu))
+        self._model.add(keras.layers.Dense(12, kernel_initializer='normal'))
+        self._model.add(keras.layers.Activation(tf.nn.relu))
+        self._model.add(keras.layers.Dense(2, activation=tf.nn.softmax))
 
         self._model.summary()
 
 
-
+    #Train model created by object
     def train(self):
 
-        self._model.compile(optimizer=tf.train.AdamOptimizer(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        self._model.compile(optimizer=tf.train.AdamOptimizer(), loss='categorical_crossentropy', metrics=['accuracy'])
         monitor = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=0, mode='auto')
         checkpointer = keras.callbacks.ModelCheckpoint(filepath=self._dataFolderPath+"optimized_weights.hdf5", verbose=0, save_best_only=True)  # save best model
         self._model.fit(self._x, self._y, callbacks=[monitor, checkpointer], verbose=0, epochs=1000)
-        self._model.load_weights(self._dataFolderPath+"optimized_weights.hdf5") # load weights from best model
+        #Use when data is large enough
+        #self._model.load_weights(self._dataFolderPath+"optimized_weights.hdf5") # load weights from best model
+
+    def predict(self, check):
+        x_in = np.array([check,])
+        print("Predicting for input:")
+        print(x_in)
+        return self._model.predict(x_in)
+
+    #Update data and train model again
+    def updateAndTrain(self,data):
+        #TODO: Add ability to save data to .csv file and retrain model on new data added
+        pass
 
     # Convert a Pandas dataframe to the x,y inputs that TensorFlow needs
     def _to_xy(self,df, target):
@@ -82,5 +94,6 @@ class predictor:
         return le.classes_
 
 if __name__ == "__main__":
-    main = predictor("Data/training.csv")
-    main.train()
+    #Testing if working
+    main = Predictor("Data/")
+    print(main.predict([234, 2345, 347, 45]))
